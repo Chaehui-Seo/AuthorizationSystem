37615d0b0b72b8ae024fe5bff9c1e686682f40a7
Chaehui Seo <sch0991@naver.com>
2022-12-08 01:33:17 +0900

    Fix: Delete unnecessary space

--- Server/routers/block.js
+++ Server/routers/block.js
@@ -3,11 +3,8 @@ const router = express.Router()
 const controller = require('../service/blockService')
 
 router.get("/block-messages", controller.loadMessages)
-
 router.post('/block-messages/create', controller.createMessage)
-
 router.put('/block-messages/response', controller.sendResponse)
-
 router.delete("/block-messages/delete", controller.deleteMessages)
 
 module.exports = router
